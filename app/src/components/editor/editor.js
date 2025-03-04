3c973065795f57b699100a39e8a50d75f2151b68
Nikita Sychev <nikitasychev@inbox.ru>
2025-03-04 22:04:55 +0500

    add backups

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -18,14 +18,16 @@ export default class Editor extends Component {
         this.currentPage = "index.html";
         this.state = {
             pageList: [],
+            backupsList: [],
             newPageName: "",
             loading: true,
         }
-        this.createNewPage = this.createNewPage.bind(this);
+      
         this.isLoading = this.isLoading.bind(this);
         this.isLoaded = this.isLoaded.bind(this);
         this.save = this.save.bind(this);
         this.init = this.init.bind(this);
+        this.restoreBackup = this.restoreBackup.bind(this);
       
     }
     //метод для того, чDOMhelper запрос на сервер осуществлялся после того, как страница отрендерилась
@@ -43,6 +45,7 @@ export default class Editor extends Component {
         this.iframe = document.querySelector('iframe');
         this.open(page, this.isLoaded);
         this.loadPageList();
+        this.loadBackupsList();
     }
 
     //метод open для открытия страницы
@@ -64,19 +67,22 @@ export default class Editor extends Component {
             .then(() => this.enableEditing())
             .then(() => this.injectStyles())
             .then(cb);
+
+            this.loadBackupsList();
     }
     //сохранение страницы на сервер
-    save(onSuccess, onError) {
-        console.log(this);
+    async save(onSuccess, onError) {
         this.isLoading();
         const newDom = this.virtualDom.cloneNode(this.virtualDom);
         DOMHelper.unwrapTextNodes(newDom);
         const html = DOMHelper.serializeDOMToString(newDom);
-        axios
+        await axios
             .post("./api/savePage.php", {pageName: this.currentPage, html})
             .then(onSuccess)
             .catch(onError)
             .finally(this.isLoaded);
+
+        this.loadBackupsList();
     }
 
     //метод для включения редактирования
@@ -111,22 +117,33 @@ export default class Editor extends Component {
             .then(res => this.setState({pageList: res.data}))
     }
 
-    createNewPage() {
+    //метод для создания списка бекапов
+    loadBackupsList() {
         axios
-            .post("./api/createNewPage.php", {"name": this.state.newPageName})
-            .then(this.loadPageList())
-            .catch(() => alert("Страница уже существует!"));
+            .get("./backups/backups.json")
+            .then(res => this.setState({backupsList: res.data.filter(backup => {
+                return backup.page === this.currentPage;
+            })
+        }))
     }
 
-
-     //метод удаления страницы
-    deletePage(page) {
-        axios
-            .post("./api/deletePage.php", {"name": page})
-            .then(this.loadPageList())
-            .catch(() => alert("Страницы не существует!"));
+    //метод восстановления резервной копии
+    restoreBackup(e, backup) {
+        console.log("Вызов метода restoreBackup, backup:", backup);
+        if (e) {
+            e.preventDefault();
+        }
+        
+        UIkit.modal.confirm("Вы действительно хотите восстановить страницу из этой резервной копии? Все несохраненные данные будут потеряны!", {labels: {ok: 'Восстановить', cancel: 'Отмена'}})
+        .then(() => {
+            this.isLoading();
+            return axios
+                .post('./api/restoreBackup.php', {"page": this.currentPage, "file": backup})
+        })
+        .then(() => {
+            this.open(this.currentPage, this.isLoaded);
+        })
     }
-
 
     //работа со спиннером
     isLoading() {
@@ -145,9 +162,11 @@ export default class Editor extends Component {
 
 
     render() {
-        const {loading, pageList} = this.state;
+        const {loading, pageList, backupsList} = this.state;
         const modal = true;
         let spinner;
+
+        console.log(backupsList);
         
         loading ? spinner = <Spinner active/> : spinner = <Spinner />
 
@@ -161,14 +180,14 @@ export default class Editor extends Component {
                 {spinner}
                
                 <div className="panel">
-                    <button className="uk-button uk-button-primary uk-margin-small-right" uk-toggle="target: #modal-open" onClick={() => this.save()}>Открыть</button>
+                    <button className="uk-button uk-button-primary uk-margin-small-right" uk-toggle="target: #modal-open">Открыть</button>
                     <button className="uk-button uk-button-primary" uk-toggle="target: #modal-save" onClick={() => this.save()}>Опубликовать</button>
                     <button className="uk-button uk-button-default" uk-toggle="target: #modal-backup">Восстановить</button>
                 </div>
                 
                 <ConfirmModal modal={modal}  target={'modal-save'} method={this.save}/>
                 <ChooseModal modal={modal}  target={'modal-open'} data={pageList} redirect={this.init}/>
-                <ChooseModal modal={modal}  target={'modal-backup'} data={pageList} redirect={this.init}/>
+                <ChooseModal modal={modal}  target={'modal-backup'} data={backupsList} redirect={this.restoreBackup}/>
             </>
         )
     }
